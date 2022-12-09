ec98f83c9b8ec58981046f06c2b15418c8481668
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2022-12-09 21:49:28 +0000

    Changing the test runner to jasmine2 so we can see the inner contests of nested unit tests.

--- gateway/jest.config.js
+++ gateway/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   forceExit: true,
+  testRunner: 'jasmine2',
   coveragePathIgnorePatterns: [
     'src/app.ts',
     'src/https.ts',
