867a37e0f9e9eb9c473be951becf2550c591e322
mhh <mike.hukiewitz@robotter.ai>
2023-10-27 22:52:14 +0200

    Fix: set correct constants for order state responses

--- hummingbot/connector/gateway/clob_perp/data_sources/mango_perpetual/mango_perpetual_constants.py
+++ hummingbot/connector/gateway/clob_perp/data_sources/mango_perpetual/mango_perpetual_constants.py
@@ -9,19 +9,21 @@ ORDER_CHAIN_PROCESSING_TIMEOUT = 5
 
 MARKETS_UPDATE_INTERVAL = 8 * 60 * 60
 
-SUPPORTED_ORDER_TYPES = [OrderType.LIMIT, OrderType.LIMIT_MAKER]
+SUPPORTED_ORDER_TYPES = [OrderType.LIMIT, OrderType.LIMIT_MAKER, OrderType.MARKET]
 SUPPORTED_POSITION_MODES = [PositionMode.ONEWAY]
 
 MANGO_DERIVATIVE_ORDER_STATES = {
-    "booked": OrderState.OPEN,
-    "partial_filled": OrderState.PARTIALLY_FILLED,
-    "filled": OrderState.FILLED,
-    "canceled": OrderState.CANCELED,
+    "CREATED": OrderState.CREATED,
+    "OPEN": OrderState.OPEN,
+    "PARTIALLY_FILLED": OrderState.PARTIALLY_FILLED,
+    "FILLED": OrderState.FILLED,
+    "CANCELLED": OrderState.CANCELED,
+    "EXPIRED": OrderState.CANCELED,
 }
 
 ORDER_SIDE_MAP = {
-    "Buy": TradeType.BUY,
-    "Sell": TradeType.SELL,
+    "BUY": TradeType.BUY,
+    "SELL": TradeType.SELL,
 }
 
 CHAIN_RPC_LIMIT_ID = "ChainRPCLimitID"
